28aade228266986e204fc88ced7816aebe16945d
zakidane <tekeste223@gmail.com>
2018-12-07 11:47:50 -0700

    vector of character structs are created, reads in from file and prints out 1 line and each character with its binary code

--- huffman.cpp
+++ huffman.cpp
@@ -6,7 +6,11 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+//the following are the two structs needed for this program.
+//one is a MinHeapNode struct and the other a character structs
+//they are essentially the same thing (that is, structs of characters that have
+//have a char data element and a frequency element). However, the MinHeap node
+//has an additional binaryCode member)
 struct MinHeapNode {
 
     // One of the input characters
@@ -14,6 +18,8 @@ struct MinHeapNode {
 
     // Frequency of the character
     int freq;
+
+    string binaryCode;
 
     // Left and right child
     MinHeapNode *left, *right;
@@ -43,14 +49,26 @@ struct character{
 };
 
 
-void printCodes(MinHeapNode* root, string str){
+void returnCodes(MinHeapNode* root, string str, vector<MinHeapNode*> v, int index){
     if(root == NULL)
         return;
-    if(root->data != '^')
-        cout << root->data << ": " << str << endl;
-    printCodes(root->left, str + "0");
-    printCodes(root->right, str + "1");
+    if(root->data != '^'){
+      root->binaryCode = str;
+
+      v.push_back(root);
+
+      cout << " root data:  " << v[index]->data << ":" << endl;
+      cout << "root binary code:  " << v[index]->binaryCode << endl;
+      cout << "root frequency: " << v[index]->freq << endl;
+      cout << "" << endl;
+      cout << "----------------------------------" << endl;
+      cout << " " << endl;
+      index++;
+    }
+    returnCodes(root->left, str + "0", v, index);
+    returnCodes(root->right, str + "1", v, index);
 }
+
 
 struct compare {
 
@@ -61,7 +79,7 @@ struct compare {
     }
 };
 
-void HuffmanCodes(char data[], int freq[], int size)
+vector<MinHeapNode*>  HuffmanCodes(char data[], int freq[], int size)
 {
     struct MinHeapNode *left, *right, *top;
 
@@ -87,7 +105,7 @@ void HuffmanCodes(char data[], int freq[], int size)
         // two nodes frequencies. Make the
         // two extracted node as left and right children
         // of this new node. Add this node
-        // to the min heap '$' is a special value
+        // to the min heap '^' is a special value
         // for internal nodes, not used
         top = new MinHeapNode('^', left->freq + right->freq);
 
@@ -99,7 +117,10 @@ void HuffmanCodes(char data[], int freq[], int size)
 
     // Print Huffman codes using
     // the Huffman tree built above
-    printCodes(minHeap.top(), "");
+    std::vector<MinHeapNode* > v;
+    int index = 0;
+    returnCodes(minHeap.top(), "", v, index);
+    return v;
 }
 
 
@@ -109,7 +130,7 @@ vector<character> putIntoArray(string line){
         vector <character>::iterator n;
         int k = 0;
         bool found = false;
-        for(n = charArray.begin(); n!= charArray.end(); n++){
+        for(n = charArray.begin(); n != charArray.end(); n++){
             if(line[i] == charArray[k].letter){
                 charArray[k].frequency++;
                 found = true;
@@ -135,11 +156,15 @@ vector<character> putIntoArray(string line){
 
 
 
+
+
 int main() {
 
-    string line;
-    cout << "INpUt LInE: " << endl;
-    getline(cin, line);
+    string line, file;
+    cout << "INpUt fILe NamE: " << endl;
+    getline(cin, file);
+    ifstream infile(file);
+    getline(infile, line,'\n');
     vector<character>CharArray = putIntoArray(line);
     int sizeLine = CharArray.size();
     cout << "size of charArray is " << sizeLine << endl;
@@ -153,7 +178,7 @@ int main() {
 
 
 
-    HuffmanCodes(heapData, heapFrequencies, sizeLine);
+      std::vector<MinHeapNode* > v = HuffmanCodes(heapData, heapFrequencies, sizeLine);
 
 	return 0;
 }
